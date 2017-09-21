3611382f999c0c5193f17f667fb043d77a1de4fc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2017-09-21 01:29:21 +0000

    Deploy nickcoutsos/jira-quicker-filters-userscript to github.com/nickcoutsos/jira-quicker-filters-userscript.git:gh-pages

---
+++ jira-quicker-filters.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name Jira Quicker Filters
+// @description Quick switching of Jira Agile Quick Filters
+// @namespace https://github.com/nickcoutsos
+// @match https://*.atlassian.net/secure/RapidBoard.jspa*
+// @downloadURL https://github.com/nickcoutsos/jira-quicker-filters-userscript/raw/gh-pages/jira-quicker-filters.user.js
+// @updateURL https://github.com/nickcoutsos/jira-quicker-filters-userscript/raw/gh-pages/jira-quicker-filters.meta.js
+// @version 0.2.0
+// @grant none
+// ==/UserScript==
